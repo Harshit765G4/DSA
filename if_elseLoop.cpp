24a94f5007c969a9942d61fafca2b32e86c081b1
gargharshit765@gmail.com <gargharshit765@gmail.com>
2025-02-14 01:33:28 +0530

    Comments_for_Refined_explaination

--- if_elseLoop.cpp
+++ if_elseLoop.cpp
@@ -1,3 +1,9 @@
+/* 
+This is Program to check if a number is greater than five.
+If the number is greater than 5, return "Yes". Otherwise, return "No".
+For example, if num = 7, the output should be "Yes".
+*/
+
 #include<iostream>
 using namespace std;
 int main(){
