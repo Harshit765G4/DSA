3d18919b2c32affb0e45774ce969fe191154f111
Harshit Garg <gargharshit765@gmail.com>
2025-02-14 01:25:18 +0530

    first commit

---
+++ if_elseLoop.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num;
+    cout<<"Enter a Number: ";
+    cin>>num;
+    if(num>5){
+        cout<<"YES"<<endl;
+    }else{
+        cout<<"NO"<<endl;
+    }
+}
