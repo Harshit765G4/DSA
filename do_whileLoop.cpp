b6128c5473dba32468a72976b9f0963772f187ab
gargharshit765@gmail.com <gargharshit765@gmail.com>
2025-02-17 23:36:50 +0530

    dowhileLoopCommenting

--- do_whileLoop.cpp
+++ do_whileLoop.cpp
@@ -3,26 +3,26 @@
 using namespace std;
 
 int main() {
-    int num; 
+    int num; // Variable to store user input
     cout << "Enter a number: ";
-    cin >> num; 
+    cin >> num; // Taking input from the user
     
-    
+    // Checking if the input number is negative
     if (num < 0) {
         cerr << "Factorial is not defined for negative numbers." << endl;
-        return 1; 
+        return 1; // Indicating an error
     }
     
-    long long fact = 1; 
-    int i = 1; 
+    long long fact = 1; // Variable to store factorial result
+    int i = 1; // Counter variable for loop
     
-    
+    // Loop to calculate factorial using do-while loop
     do {
         fact *= i;
         i++;
     } while (i <= num);
     
-    
+    // Displaying the factorial result
     cout << "Factorial of " << num << " is " << fact << endl;
     return 0;
 }
