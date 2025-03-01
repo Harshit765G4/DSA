b463eaca57b43a3d20989e13f2cc2b2b8192141d
gargharshit765@gmail.com <gargharshit765@gmail.com>
2025-03-02 03:49:31 +0530

    Brute force

--- LeetCode/P1_TwoSum.cpp
+++ LeetCode/P1_TwoSum.cpp
@@ -1,6 +1,17 @@
 #include<iostream>
 using namespace std;
 int main(){
-    
+    int nums[] = {2, 7, 11, 15};
+    int target = 9;
+    int i,j;
+    for(i=0;i<4;i++){
+        for(j=i+1;j<4;j++){
+            if(i==j){
+                continue;
+            } else if(nums[i]+nums[j]==target){
+                cout<<i<<" "<<j<<endl;
+            }
+        }
+    }
     return 0;
 }
