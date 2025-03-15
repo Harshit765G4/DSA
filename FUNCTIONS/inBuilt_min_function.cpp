7f36c755d8306876ff9ccd5194e29c36e777f679
gargharshit765@gmail.com <gargharshit765@gmail.com>
2025-03-15 20:47:58 +0530

    implementation

---
+++ FUNCTIONS/inBuilt_min_function.cpp
@@ -0,0 +1,8 @@
+int main(){
+        int num1, num2;
+        cout<< "Enter two Numbers: ";
+        cin>>num1>>num2;
+        int minimum = min(num1, num2);
+        cout <<minimum<<endl;
+        return 0;
+    }
