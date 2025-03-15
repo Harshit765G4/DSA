817e3592113186874a581c635185ac05e3bae8d7
gargharshit765@gmail.com <gargharshit765@gmail.com>
2025-03-15 20:42:55 +0530

    inbuilt_max_function

---
+++ FUNCTIONS/inBuilt_max_function.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+        int num1, num2;
+        cout<< "Enter two Numbers: ";
+        cin>>num1>>num2;
+        int maximum = max(num1, num2);
+        cout <<maximum<<endl;
+        return 0;
+    }
