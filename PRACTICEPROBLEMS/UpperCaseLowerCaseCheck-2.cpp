5a9b417d18a232c7137be0ca82796c0093c81f5c
gargharshit765@gmail.com <gargharshit765@gmail.com>
2025-03-19 23:46:25 +0530

    Logic

--- PRACTICEPROBLEMS/UpperCaseLowerCaseCheck-2.cpp
+++ PRACTICEPROBLEMS/UpperCaseLowerCaseCheck-2.cpp
@@ -3,6 +3,16 @@ using namespace std;
 
 int main(){
 
+    char ch;
+    cout<<"Enter the character"<<endl;
+    cin>>ch;
+    if(ch>=65 && ch<=90){
+        cout<<"UpperCase"<<endl;
+    }else if(ch>=97 && ch<=122){
+        cout<<"LowerCase"<<endl;
+    }else{
+        cout<<"Invalid character!"<<endl;
+    }
     
     return 0;
 }
