e66febbfc4a45bfbeb640ed08c15c465416e0f81
gargharshit765@gmail.com <gargharshit765@gmail.com>
2025-03-20 21:44:00 +0530

    Logic

--- PRACTICEPROBLEMS/SquareStarPatternL2.cpp
+++ PRACTICEPROBLEMS/SquareStarPatternL2.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main(){
 
     for(int i=1;i<=4;i++){
-        for(int j=0;j<=4;j++){
+        for(int j=1;j<=4;j++){
             cout<<"*"<<" ";
         }
         cout<<endl;
