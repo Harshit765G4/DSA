225c0998ae96956757b4c9fa1c7cd045f22d056e
gargharshit765@gmail.com <gargharshit765@gmail.com>
2025-03-25 21:59:04 +0530

    Logic

--- PATTERNS/Triangle.cpp
+++ PATTERNS/Triangle.cpp
@@ -5,6 +5,12 @@ int main(){
     int n;
     cout<<"Enter the number : ";
     cin>>n;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<=i;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
 
     return 0;
 }
