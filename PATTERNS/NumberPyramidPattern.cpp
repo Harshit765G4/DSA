d9576a1b629492efd7952c934ccc117dad234d16
gargharshit765@gmail.com <gargharshit765@gmail.com>
2025-03-29 23:51:22 +0530

    Logic for Num2 remaining half pyramid

--- PATTERNS/NumberPyramidPattern.cpp
+++ PATTERNS/NumberPyramidPattern.cpp
@@ -7,9 +7,13 @@ int main(){
         for(int j=0;j<n-i-1;j++){
             cout<<" ";
         }
-        for(int j=1;j<i+1;j++){
+        for(int j=1;j<=i+1;j++){
             cout<<j;
         }
+        for(int j=i;j>=1;j--){
+            cout<<j;
+        }
+        cout<<endl;
     }
     return 0;
 }
