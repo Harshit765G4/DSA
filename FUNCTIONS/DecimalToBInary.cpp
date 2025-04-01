5d40fab4f61f8bef8b21b2bfe81925502efca1ef
gargharshit765@gmail.com <gargharshit765@gmail.com>
2025-04-01 23:55:46 +0530

    Function Logic

--- FUNCTIONS/DecimalToBInary.cpp
+++ FUNCTIONS/DecimalToBInary.cpp
@@ -4,6 +4,12 @@ using namespace std;
 int decToBinary(int decNum){
     int ans = 0;
     int pow = 1;
+    while(decNum > 0){
+        int rem = decNum % 2;
+        decNum = decNum / 2;
+        ans += (rem*pow);
+        pow *= 10;
+    }
 }
 
 int main(){
